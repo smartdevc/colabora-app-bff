6ab49c1baf86f2282805f07bc4938bf4f2a42de5
Sebastian Droguett <sdroguett10@gnail.com>
2024-02-11 23:04:18 -0300

    Project creation

---
+++ src/main/java/cl/colabora/colaboraappbff/ColaboraAppBffApplication.java
@@ -0,0 +1,13 @@
+package cl.colabora.colaboraappbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboraAppBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColaboraAppBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/colabora/colaboraappbff/ColaboraAppBffApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.colabora.colaboraappbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboraAppBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
